cea612dd6b7130104a98f715d4913c66f318d78b
Pauline ATTAL <pauline.attal8@gmail.com>
2023-03-05 21:27:59 +0100

    update regex for extract info in test file, and update code for filters in dash

--- chellengeApp/pages/page1.py
+++ chellengeApp/pages/page1.py
@@ -2,23 +2,21 @@ import pandas as pd
 from dash import html, dcc, Input, Output, callback
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import plotly.graph_objects as go
 
-df = pd.read_csv(r'C:\Users\pauli\Documents\M2\secu\challenge\Challenge_security\chellengeApp\pages\data_test.csv', sep=',')
+df = pd.read_csv(r'C:\Users\pauli\Documents\M2\secu\challenge\Challenge_security\chellengeApp\data\data_test.csv', sep=',')
 
 ###definition des gaphiques fig
 
 # Créer un histogramme des requêtes par heure
-requests_per_hour = df.groupby(df['datetime'].str[:2]).size().reset_index(name='count')
-fig1 = px.bar(requests_per_hour, x='datetime', y='count', title='Requêtes par heure')
+requests_per_hour = df.groupby(df['minute']).size().reset_index(name='count')
+fig1 = px.bar(requests_per_hour, x='minute', y='count', title='Requêtes par heure')
 
 # Créer un graphique à barres pour les adresses IP avec le plus grand nombre de requêtes
 ip_counts = df['local_ip'].value_counts().reset_index()
 ip_counts.columns = ['local_ip', 'count']
 top_ips = ip_counts.head(10)
 fig2 = px.bar(top_ips, x='local_ip', y='count', title='Les 10 adresses IP les plus fréquentes')
-
-
-
 
 card1 = dbc.Card(
     dbc.CardBody(
@@ -52,39 +50,40 @@ def layout():
             html.H1("Page 1"),
             html.Hr(),
             dbc.Row(
-                [
-                    dbc.Col(
-                        dcc.DatePickerRange(
-                            id='date-picker',
-                            display_format='DD/MM/YYYY',
-                            start_date_placeholder_text='Start Date',
-                            end_date_placeholder_text='End Date',
-                        ),
-                        width=3,
+            [
+                dbc.Col(
+                    dcc.DatePickerRange(
+                        id='date-picker',
+                        display_format='DD/MM/YYYY',
+                        start_date_placeholder_text='Start Date',
+                        end_date_placeholder_text='End Date',
                     ),
-                    dbc.Col(
-                        dcc.Dropdown(
-                            id='dropdown-code',
-                            options=[
-                                {'label': '200', 'value': '200'},
-                                {'label': '404', 'value': '404'},
-                                {'label': '500', 'value': '500'}
-                            ],
-                            placeholder='Select a Status Code',
-                        ),
-                        width=3,
+                    width=3,
+                ),
+                dbc.Col(
+                    dcc.Dropdown(
+                        id='status-dropdown',
+                        options=[
+                            {'label': '200', 'value': '200'},
+                            {'label': '404', 'value': '404'},
+                            {'label': '500', 'value': '500'}
+                        ],
+                        placeholder='Select a Status Code',
                     ),
-                    dbc.Col(
-                        dcc.Input(
-                            id='ip-input',
-                            type='text',
-                            placeholder='Enter an IP Address',
-                        ),
-                        width=3,
+                    width=3,
+                ),
+                dbc.Col(
+                    dcc.Input(
+                        id='ip-input',
+                        type='text',
+                        placeholder='Enter an IP Address',
                     ),
-                ],
-                className="mb-2",
-            ),
+                    width=3,
+                ),
+            ],
+            className="mb-2",
+        ),
+
             dbc.Row(
                 [
                     dbc.Col(
@@ -102,104 +101,40 @@ def layout():
         fluid=True,
     )
 
-dcc.Dropdown(
-    id='dropdown-code',
-    options=[
-        {'label': '200', 'value': '200'},
-        {'label': '404', 'value': '404'},
-        {'label': '500', 'value': '500'},
-        # Ajoutez d'autres codes de retour si besoin
-    ],
-    value='200'
-)
 
-from datetime import date
-from datetime import timedelta
-dcc.DatePickerRange(
-    id='date-picker-range',
-    min_date_allowed=date(2022, 1, 1),
-    max_date_allowed=date.today(),
-    initial_visible_month=date.today(),
-    start_date=date.today() - timedelta(7),
-    end_date=date.today()
-)
-
-dcc.Input(
-    id='input-ip',
-    type='text',
-    placeholder='Entrez une adresse IP'
-)
-
-import plotly.express as px
-
-# Mise à jour du premier graphe en fonction des filtres appliqués
 @callback(
-    Output('graph-1', 'figure'),
-    [Input('filter-code', 'value'),
-     Input('filter-date', 'start_date'),
-     Input('filter-date', 'end_date'),
-     Input('filter-ip', 'value')])
-def update_graphes_1(code, start_date, end_date, ip):
-    # Filtrage des données en fonction des valeurs des filtres
-    filtered_df = df[(df['code'] == code) &
-                     (df['date'].dt.date >= start_date) &
-                     (df['date'].dt.date <= end_date) &
-                     (df['local_ip'].isin(ip))]
-    
-    # Calcul des données à afficher sur le graphe
-    data = filtered_df.groupby('method')[['size']].sum().reset_index()
-    
-    # Création et renvoi de la figure
-    fig = px.bar(data, x='method', y='size', labels={'size': 'Taille totale (en octets)'})
-    fig.update_layout(title='Taille totale des requêtes par méthode HTTP',
-                      xaxis_title='Méthode HTTP', yaxis_title='Taille totale (en octets)')
-    return fig
+    [Output('graph1', 'figure'),
+     Output('graph2', 'figure')],
+    [Input('date-picker', 'start_date'),
+     Input('date-picker', 'end_date'),
+     Input('status-dropdown', 'value'),
+     Input('ip-input', 'value')])
+def update_figures(start_date, end_date, status, ip):
+    filtered_df = df.copy()
+    if start_date and end_date:
+        filtered_df = filtered_df[(filtered_df['time'] >= start_date) & (filtered_df['time'] <= end_date)]
+    if status:
+        filtered_df = filtered_df[filtered_df['status'] == int(status)]
+    if ip:
+        filtered_df = filtered_df[filtered_df['local_ip'] == ip]
 
+    # Update fig1
+    if not filtered_df.empty:
+        requests_per_minute = filtered_df.groupby(filtered_df['minute']).size().reset_index(name='count')
+        fig1 = px.bar(requests_per_minute, x='minute', y='count', title='Requêtes par minute')
+    else:
+        fig1 = px.bar(df.groupby(df['minute']).size().reset_index(name='count'), x='minute', y='count', title='Requêtes par minute')
 
-# Mise à jour du deuxième graphe en fonction des filtres appliqués
-@callback(
-    Output('graph-2', 'figure'),
-    [Input('filter-code', 'value'),
-     Input('filter-date', 'start_date'),
-     Input('filter-date', 'end_date'),
-     Input('filter-ip', 'value')])
-def update_graphes_2(code, start_date, end_date, ip):
-    # Filtrage des données en fonction des valeurs des filtres
-    filtered_df = df[(df['code'] == code) &
-                     (df['date'].dt.date >= start_date) &
-                     (df['date'].dt.date <= end_date) &
-                     (df['local_ip'].isin(ip))]
-    
-    # Calcul des données à afficher sur le graphe
-    data = filtered_df.groupby('local_ip')[['size']].sum().reset_index()
-    
-    # Création et renvoi de la figure
-    fig = px.pie(data, values='size', names='ip')
-    fig.update_layout(title='Répartition des requêtes par adresse IP',
-                      xaxis_title='Adresse IP', yaxis_title='Taille totale (en octets)')
-    return fig
+    # Update fig2
+    if not filtered_df.empty:
+        ip_counts = filtered_df['local_ip'].value_counts().reset_index()
+        ip_counts.columns = ['local_ip', 'count']
+        top_ips = ip_counts.head(10)
+        fig2 = px.bar(top_ips, x='local_ip', y='count', title='Les 10 adresses IP les plus fréquentes')
+    else:
+        ip_counts = df['local_ip'].value_counts().reset_index()
+        ip_counts.columns = ['local_ip', 'count']
+        top_ips = ip_counts.head(10)
+        fig2 = px.bar(top_ips, x='local_ip', y='count', title='Les 10 adresses IP les plus fréquentes')
 
-
-# Mise à jour du troisième graphe en fonction des filtres appliqués
-@callback(
-    Output('graph-3', 'figure'),
-    [Input('filter-code', 'value'),
-     Input('filter-date', 'start_date'),
-     Input('filter-date', 'end_date'),
-     Input('filter-ip', 'value')])
-def update_graphes_3(code, start_date, end_date, ip):
-    # Filtrage des données en fonction des valeurs des filtres
-    filtered_df = df[(df['code'] == code) &
-                     (df['date'].dt.date >= start_date) &
-                     (df['date'].dt.date <= end_date) &
-                     (df['local_ip'].isin(ip))]
-    
-    # Calcul des données à afficher sur le graphe
-    data = filtered_df.groupby(pd.Grouper(key='date', freq='1H')).size().reset_index(name='count')
-    
-    # Création et renvoi de la figure
-    fig = px.line(data, x='date', y='count')
-    fig.update_layout(title='titre',
-                      xaxis_title='x', yaxis_title='y')
-    return fig
-
+    return fig1, fig2
