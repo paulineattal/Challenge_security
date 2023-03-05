c1b5cfa02b49742ea2b5b934803d508acdee08a3
Pauline ATTAL <pauline.attal8@gmail.com>
2023-03-05 19:01:48 +0100

    make txt file in df with good fields

--- chellengeApp/pages/page1.py
+++ chellengeApp/pages/page1.py
@@ -12,10 +12,10 @@ requests_per_hour = df.groupby(df['datetime'].str[:2]).size().reset_index(name='
 fig1 = px.bar(requests_per_hour, x='datetime', y='count', title='Requêtes par heure')
 
 # Créer un graphique à barres pour les adresses IP avec le plus grand nombre de requêtes
-ip_counts = df['ip'].value_counts().reset_index()
-ip_counts.columns = ['ip', 'count']
+ip_counts = df['local_ip'].value_counts().reset_index()
+ip_counts.columns = ['local_ip', 'count']
 top_ips = ip_counts.head(10)
-fig2 = px.bar(top_ips, x='ip', y='count', title='Les 10 adresses IP les plus fréquentes')
+fig2 = px.bar(top_ips, x='local_ip', y='count', title='Les 10 adresses IP les plus fréquentes')
 
 
 
@@ -89,11 +89,11 @@ def layout():
                 [
                     dbc.Col(
                         card1,
-                        width=3,
+                        width=6,
                     ),
                     dbc.Col(
                         card2,
-                        width=3,
+                        width=6,
                     )
                 ],
                 className="mb-2",
@@ -144,7 +144,7 @@ def update_graphes_1(code, start_date, end_date, ip):
     filtered_df = df[(df['code'] == code) &
                      (df['date'].dt.date >= start_date) &
                      (df['date'].dt.date <= end_date) &
-                     (df['ip'].isin(ip))]
+                     (df['local_ip'].isin(ip))]
     
     # Calcul des données à afficher sur le graphe
     data = filtered_df.groupby('method')[['size']].sum().reset_index()
@@ -168,10 +168,10 @@ def update_graphes_2(code, start_date, end_date, ip):
     filtered_df = df[(df['code'] == code) &
                      (df['date'].dt.date >= start_date) &
                      (df['date'].dt.date <= end_date) &
-                     (df['ip'].isin(ip))]
+                     (df['local_ip'].isin(ip))]
     
     # Calcul des données à afficher sur le graphe
-    data = filtered_df.groupby('ip')[['size']].sum().reset_index()
+    data = filtered_df.groupby('local_ip')[['size']].sum().reset_index()
     
     # Création et renvoi de la figure
     fig = px.pie(data, values='size', names='ip')
@@ -192,7 +192,7 @@ def update_graphes_3(code, start_date, end_date, ip):
     filtered_df = df[(df['code'] == code) &
                      (df['date'].dt.date >= start_date) &
                      (df['date'].dt.date <= end_date) &
-                     (df['ip'].isin(ip))]
+                     (df['local_ip'].isin(ip))]
     
     # Calcul des données à afficher sur le graphe
     data = filtered_df.groupby(pd.Grouper(key='date', freq='1H')).size().reset_index(name='count')
