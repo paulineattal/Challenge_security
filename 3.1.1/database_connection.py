6428d076f5738c5ef1de2de42164823a440ec2e3
Louison Papazian <louison.papazian@outlook.fr>
2023-03-06 21:43:56 +0100

    Update

--- 3.1.1/database_connection.py
+++ 3.1.1/database_connection.py
@@ -1,29 +1,14 @@
 import pandas as pd
+import pandasql as ps
+from datetime import datetime
+import pickle
+import joblib
 import mysql.connector as sql
 
 
-# Connect to the database
-db_connection = sql.connect( host='192.168.1.21',
-                             database='Logs_fw', 
-                             user='root', 
-                             password='mypass123')
-
-
-# Create a cursor object
+db_connection = sql.connect(host='192.168.1.21', database='Logs_fw', user='root', password='mypass123')
 db_cursor = db_connection.cursor()
-
-# Execute the query
 db_cursor.execute('SELECT * FROM FW')
-
-# Fetch all the records
 table_rows = db_cursor.fetchall()
-
-# Close the cursor
-db_cursor.close()
-
-# Close the database connection
-db_connection.close()
-
-# Create a dataframe from the database table
-dfdb = pd.DataFrame(table_rows)
-dfdb.columns=['date', 'ipsrc', 'ipdst', 'port','proto', 'action', 'policyid']
+db = pd.DataFrame(table_rows)
+db.columns=['date', 'ipsrc', 'ipdst', 'port','proto', 'action', 'policyid']
