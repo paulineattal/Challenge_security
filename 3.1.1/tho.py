790b4b06eee3324bd1e1c131ce2376d3ba0d20e9
Tho HOANG <114235978+thohoang87@users.noreply.github.com>
2023-03-07 10:51:49 +0100

    Update tho.py

--- 3.1.1/tho.py
+++ 3.1.1/tho.py
@@ -5,81 +5,19 @@ import os
 import plotly.express as px
 os.chdir("/Users/dangnguyenviet/Desktop/projet-securite")
 
-# histogramme événement par heure:
-def histo_evenement_heure(df):
-    df['hour_of_event'] = pd.to_datetime(df['datetime']).dt.hour
-
-    eventdata = pd.DataFrame({'hour_of_event': df['hour_of_event']})
-    eventdata['Horaire'] = eventdata['hour_of_event'].apply(lambda x: 'Horaires ouvrés' if 7 <= x <= 18 else 'Horaires non ouvrés')
-
-    fig = px.histogram(eventdata, x='hour_of_event', color='Horaire', nbins=24,
-                   labels={'hour_of_event': 'Heure', 'count': 'Somme'},
-                   category_orders={'Horaire': ['Horaires ouvrés', 'Horaires non ouvrés']},
-                   color_discrete_sequence=px.colors.qualitative.Pastel)
-    fig.update_layout(title='Evenements par heure', yaxis_title='Somme', xaxis_title='',
-                  xaxis=dict(dtick=1, tick0=0, range=[0, 24]), bargap=0.1)
-    fig.update_traces(hovertemplate='Horaire: %{legendgroup}<br>Heure: %{x}<br>Count: %{y}')
-    fig.show()
-    
-histo_evenement_heure(df)
-
-# échantillonnage
-f = open("log_fw_3.csv","r")
-lst = f.readlines()
-print(len(lst)-1)
-#fermeture
-f.close()
-
-def extraction(source,sortie,n,N):
-    #fichier source
-    fin = open(source,"r")
-    #fichier de sortie
-    fout = open(sortie,"w")
-    #en-tête
-    s = fin.readline()
-    fout.write(s)
-    #compteur de vérification - pas absolument nécessaire
-    cpt = 0
-    #boucler
-    while (n > 0):
-        #lire la ligne
-        s = fin.readline()
-        #inclure dans la sortie ?
-        if (N * np.random.rand() <= n):
-            #oui
-            fout.write(s)
-            #un de moins à insérer
-            n = n - 1
-            #incrémentation compteur
-            cpt = cpt + 1
-        #une entrée a été traitée
-        N = N - 1
-    #fin du tant que
-    fin.close()
-    fout.close()
-    #return
-    return cpt
-
-N = len(lst)-1
-
-source = "log_fw_3.csv"
-sortie = "log_fw_3_extract.csv"
-n = 5000
-extraction(source, sortie,n,N)
-
-df = pd.read_table("log_fw_3_extract.csv",sep=";",header=None)
-df.drop([8,9],axis=1,inplace=True)
-
-df.columns = ["datetime","ipsrc","ipdst","proto","portsrc","portdst","policyid","action","numproto"]
 
 # CAH
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+df = pd.read_table("FW.csv",sep=",",header=0)
+df.dstport.astype("float")
+df.policyid.astype("float")
+df.info()
+df_actif = df[["dstport","proto","action","policyid"]]
 
-df_actif = df.iloc[:,3:]
 df_actif = pd.get_dummies(df_actif, columns=df_actif.select_dtypes('object').columns)
-
+print(df_actif.info())
 def matrice_lien(df):
     #générer la matrice des liens
     dist_ind_data =linkage(df.values,method='ward',metric='euclidean')
@@ -88,9 +26,8 @@ def matrice_lien(df):
 dist_ind_data = matrice_lien(df_actif)
 
 #affichage du dendrogramme
-plt.title("CAH") 
-dendrogram(dist_ind_data,orientation='right',color_threshold=0) 
-plt.show()
+import seaborn as sns
+sns.clustermap(dist_ind_data)
 
 # afficher chaque groupe:
 def groupe_cah(matrice,seuil,nb_groupe):
